dba821d23e4804b2533437fa89462d3359111003
Рустам Хашимов <rkkhashimov@sberbank.ru>
2024-06-01 01:32:33 +0300

    added changes

--- src/mocks/users.mock.js
+++ src/mocks/users.mock.js
@@ -2,8 +2,8 @@ export const usersMock = [
     {
         login: 'test',
         password: 'testpassword',
-        firstName: 'Павел',
-        lastName: 'Кондратьев',
+        firstName: 'Рустам',
+        lastName: 'Хашимов',
         photo: 'https://i.pinimg.com/736x/fa/68/26/fa68266e4bcac979a85327dfe621938c.jpg'
     },
 ]
